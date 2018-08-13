1a5d4397545e5848f9813b13134fde15acfc3172
Cedrick Ngeja <cedrickngeja@gmail.com>
2018-08-13 08:01:59 +0100

    add ssr files

---
+++ modules/routes/Foot.js
@@ -0,0 +1,29 @@
+import React        from 'react'
+import { Link }     from 'react-router-dom'
+
+const Foot = ({}) => {
+    return (
+        <div className="foo">
+            <div className="foo-a">
+                <div className="foo-lft">
+                    <div className="foo-opt">
+                        <Link to="/about">À propos de la Traduction</Link>
+                    </div>
+                    <div className="foo-opt">
+                        <Link to="/privacy">Confidentialité et conditions d'utilisation</Link>
+                    </div>
+                </div>
+                <div className="foo-rght">
+                    <div className="foo-opt">
+                        <Link to="/help">Aide</Link>
+                    </div>
+                    <div className="foo-opt">
+                        @copyright . 2018
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Foot
---
+++ routes/renderFullPage.js
@@ -0,0 +1,73 @@
+var qs                 =  require('qs') // Add this at the top of the file
+var React              =  require('react')
+var { renderToString } =  require('react-dom/server')
+import { renderStylesToString } from 'emotion-server'
+var {
+    createStore,
+    combineReducers,
+    applyMiddleware,
+    compose
+}                      = require('redux')
+var formReducer        = require('redux-form')
+var thunk              = require('redux-thunk')
+var reducers           = require('../modules/reducers')
+var { Provider }       = require('react-redux')
+var { StaticRouter }   = require('react-router')
+
+var template           = require('./template').default
+var App                = require('../modules/App').default
+var Root               = require('../modules/Root').default
+const createHistory    = require('history/createMemoryHistory').default;
+
+function renderFullPage(req, res, params) {
+    const context = {};
+    const { preloadedState, url, title } = params;
+
+    const history = createHistory({
+        initialEntries: [ url ],   // The initial URLs in the history stack
+        initialIndex: 0,                // The starting index in the history stack
+        // keyLength: 6,                   // The length of location.key
+        // A function to use to confirm navigation with the user. Required
+        // if you return string prompts from transition hooks (see below)
+        getUserConfirmation: null
+    });
+
+
+    // Create a new Redux store instance
+    const store = createStore(
+        combineReducers({
+            ...reducers,
+            form: formReducer
+        }),
+        preloadedState
+    )
+
+    // history={history}
+    // Render the component to a string
+    // renderStylesToString from react-select doc for SSR
+    const body = renderStylesToString(renderToString(
+        <Provider store={store}>
+            <StaticRouter
+                basename=''
+                location={url}
+                context={context}>
+                <App>
+                    <Root location={{}} />
+                </App>
+            </StaticRouter>
+        </Provider>
+    ))
+
+    // Grab the initial state from our Redux store
+    const finalState = store.getState()
+
+    // Send the rendered page back to the client
+    res.send(template({
+        body,
+        title,
+        preloadedState: finalState
+    }))
+}
+
+module.exports = renderFullPage;
+// export default renderFullPage
