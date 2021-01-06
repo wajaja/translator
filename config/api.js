8a77fd6d988db7cf32c3b0a23ce029162b93561b
Cedrick Ngeja <cedrickngeja@gmail.com>
2021-01-07 00:09:06 +0100

    fix webpack-prod content 1

--- config/api.js
+++ config/api.js
@@ -1,1 +1,1 @@
-export const BASE_PATH = process.env.NODE_ENV === 'production' ? "https://traduction.xyz" : "http://127.0.0.1:3000";
+export const BASE_PATH = process.env.NODE_ENV === 'production' ? "https://civiliser.com" : "http://127.0.0.1:3000";
